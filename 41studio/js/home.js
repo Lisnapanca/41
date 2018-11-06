60cff6a814e215db24fb2314cc4e7453adc7baea
bitqueen <lisnaebit@gmail.com>
2018-11-06 16:16:27 +0700

    styling

--- 41studio/js/home.js
+++ 41studio/js/home.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
   $("#web").click(function(){
       $("#web-part").show();
+      $("#web-arrow").show();
+      $("#mobile-arrow").hide();
+      $("#desktop-arrow").hide();
+      $("#design-arrow").hide();
+      $("#category-bar").css('background-color', '#f2ac00');
       $("#mobile-part").hide();
       $("#desktop-part").hide();
       $("#design-part").hide();
@@ -8,6 +13,12 @@ $(document).ready(function(){
   $("#mobile").click(function(){
       $("#web-part").hide();
       $("#mobile-part").show();
+      $("#web-arrow").hide();
+      $("#mobile-arrow").show();
+      $("#desktop-arrow").hide();
+      $("#design-arrow").hide();
+      $("#mobile-arrow").css('border-bottom', '5px solid #50a5f5');
+      $("#category-bar").css('background-color', '#50a5f5');
       $("#desktop-part").hide();
       $("#design-part").hide();
   });
@@ -15,6 +26,12 @@ $(document).ready(function(){
       $("#web-part").hide();
       $("#mobile-part").hide();
       $("#desktop-part").show();
+      $("#web-arrow").hide();
+      $("#mobile-arrow").hide();
+      $("#desktop-arrow").show();
+      $("#design-arrow").hide();
+      $("#desktop-arrow").css('border-bottom', '5px solid #ed5150');
+      $("#category-bar").css('background-color', '#ed5250');
       $("#design-part").hide();
   });
   $("#design").click(function(){
@@ -22,9 +39,14 @@ $(document).ready(function(){
       $("#mobile-part").hide();
       $("#desktop-part").hide();
       $("#design-part").show();
+      $("#web-arrow").hide();
+      $("#mobile-arrow").hide();
+      $("#desktop-arrow").hide();
+      $("#design-arrow").show();
+      $("#design-arrow").css('border-bottom', '5px solid #66bb69');
+      $("#category-bar").css('background-color', '#66bb69');
   });
 });
-
 $(document).ready(function () {
     $('.owl-carousel-header').owlCarousel({
     loop:true,
