476e7aac0e1690a7a8933480c8e4622ec73f16ea
bitqueen <lisnaebit@gmail.com>
2018-11-07 10:42:28 +0700

    styling

--- 41studio/js/home.js
+++ 41studio/js/home.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
       $("#mobile-arrow").hide();
       $("#desktop-arrow").hide();
       $("#design-arrow").hide();
+      $("#web-button").css('color', '#f2ac00');
       $("#category-bar").css('background-color', '#f2ac00');
       $("#mobile-part").hide();
       $("#desktop-part").hide();
@@ -17,6 +18,8 @@ $(document).ready(function(){
       $("#mobile-arrow").show();
       $("#desktop-arrow").hide();
       $("#design-arrow").hide();
+      $("#mobile-button").css('color', '#50a5f5');
+      $("#mobile-button").css('border-color','#50a5f5');
       $("#mobile-arrow").css('border-bottom', '5px solid #50a5f5');
       $("#category-bar").css('background-color', '#50a5f5');
       $("#desktop-part").hide();
@@ -30,6 +33,8 @@ $(document).ready(function(){
       $("#mobile-arrow").hide();
       $("#desktop-arrow").show();
       $("#design-arrow").hide();
+      $("#desktop-button").css('color', '#ed5150');
+      $("#desktop-button").css('border-color','#ed5150');
       $("#desktop-arrow").css('border-bottom', '5px solid #ed5150');
       $("#category-bar").css('background-color', '#ed5250');
       $("#design-part").hide();
@@ -43,8 +48,56 @@ $(document).ready(function(){
       $("#mobile-arrow").hide();
       $("#desktop-arrow").hide();
       $("#design-arrow").show();
+      $("#design-button").css('color', '#66bb69');
+      $("#design-button").css('border-color','#66bb69');
       $("#design-arrow").css('border-bottom', '5px solid #66bb69');
       $("#category-bar").css('background-color', '#66bb69');
+  });
+});
+$(document).ready(function(){
+  $("#people1").click(function(){
+      $("#people1-part").show();
+      $("#people1-arrow").show();
+      $("#people2-arrow").hide();
+      $("#people3-arrow").hide();
+      $("#people4-arrow").hide();
+      $("#people1-arrow").css('border-bottom', '5px solid #50a5f5');
+      $("#people2-part").hide();
+      $("#people3-part").hide();
+      $("#people4-part").hide();
+  });
+  $("#people2").click(function(){
+      $("#people1-part").hide();
+      $("#people2-part").show();
+      $("#people1-arrow").hide();
+      $("#people2-arrow").show();
+      $("#people3-arrow").hide();
+      $("#people4-arrow").hide();
+      $("#people2-arrow").css('border-bottom', '5px solid #50a5f5');
+      $("#people3-part").hide();
+      $("#people4-part").hide();
+  });
+  $("#people3").click(function(){
+      $("#people1-part").hide();
+      $("#people2-part").hide();
+      $("#people3-part").show();
+      $("#people1-arrow").hide();
+      $("#people2-arrow").hide();
+      $("#people3-arrow").show();
+      $("#people4-arrow").hide();
+      $("#people3-arrow").css('border-bottom', '5px solid #50a5f5');
+      $("#people4-part").hide();
+  });
+  $("#people4").click(function(){
+      $("#people1-part").hide();
+      $("#people2-part").hide();
+      $("#people3-part").hide();
+      $("#people4-part").show();
+      $("#people1-arrow").hide();
+      $("#people2-arrow").hide();
+      $("#people3-arrow").hide();
+      $("#people4-arrow").show();
+      $("#people4-arrow").css('border-bottom', '5px solid #50a5f5');
   });
 });
 $(document).ready(function () {
--- 41studio/js/service.js
+++ 41studio/js/service.js
@@ -1,24 +1,34 @@
-$( "#front-end-btn" ).click(function() {
-      $("#front-end").show();
-      $("#mobile-dev").hide();
-      $("#back-end").hide();
-      $("#desktop-dev").hide();
-    });
-    $( "#mobile-dev-btn" ).click(function() {
-      $("#front-end").hide();
-      $("#mobile-dev").show();
-      $("#back-end").hide();
-      $("#desktop-dev").hide();
-    });
-    $( "#back-end-btn" ).click(function() {
-      $("#front-end").hide();
-      $("#mobile-dev").hide();
-      $("#back-end").show();
-      $("#desktop-dev").hide();
-    });
-    $( "#desktop-dev-btn" ).click(function() {
-      $("#front-end").hide();
-      $("#mobile-dev").hide();
-      $("#back-end").hide();
-      $("#desktop-dev").show();
-    });
+// $( "#front-end-btn" ).click(function() {
+//       $("#front-end").show();
+//       $("#mobile-dev").hide();
+//       $("#back-end").hide();
+//       $("#desktop-dev").hide();
+//     });
+//     $( "#mobile-dev-btn" ).click(function() {
+//       $("#front-end").hide();
+//       $("#mobile-dev").show();
+//       $("#back-end").hide();
+//       $("#desktop-dev").hide();
+//     });
+//     $( "#back-end-btn" ).click(function() {
+//       $("#front-end").hide();
+//       $("#mobile-dev").hide();
+//       $("#back-end").show();
+//       $("#desktop-dev").hide();
+//     });
+//     $( "#desktop-dev-btn" ).click(function() {
+//       $("#front-end").hide();
+//       $("#mobile-dev").hide();
+//       $("#back-end").hide();
+//       $("#desktop-dev").show();
+//     });
+$(document).ready(function(){
+  $("#extended-team-button").click(function(){
+    $("#project-based-content").hide();
+    $("#extended-team-content").show();
+  });
+  $("#project-based-button").click(function(){
+    $("#project-based-content").show();
+    $("#extended-team-content").hide();
+  });
+});
